0371da25f8202ce4f25f05e9c771e2a47f476c4d
pltgst <selimovsultan@gmail.com>
2024-01-29 19:34:15 +0300

    deleted env varible usage

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -2,13 +2,17 @@ import os
 
 
 class Config:
-    #SECRET_KEY = '209a95d89e17faeb245479499776f6d6'
-    #SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = '209a95d89e17faeb245479499776f6d6'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    #SECRET_KEY = os.environ.get('SECRET_KEY')
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    #MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    #MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = 'flaskproj997@gmail.com'
+    MAIL_PASSWORD = 'aiguozgldoeeyccd'
+
+    
